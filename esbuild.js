3f9a7d67cac2ff3f13d062da02f1bedf130a0314
David Hirtz <david.hirtz@me.com>
2023-04-07 08:38:44 +0100

    Removed gulp for esbuild

--- esbuild.js
+++ esbuild.js
@@ -2,35 +2,11 @@ import * as esbuild from 'esbuild'
 
 // Use --watch flag to watch for changes and rebuild automatically.
 const isWatch = process.argv.slice(2).includes('--watch');
-let cssStartTime;
-
-const watchPlugin = {
-    name: 'watch-plugin',
-    setup(build) {
-        build.onStart(() => {
-            cssStartTime = Date.now();
-        });
-
-        build.onEnd((result) => {
-            if (result.errors.length) {
-                console.log(result.errors);
-            }
-
-            console.log(`Compiled scripts with esbuild (${esbuild.version}) in ${Date.now() - cssStartTime}ms`);
-        });
-    }
-};
 
 let context = await esbuild.context({
-    entryPoints: [
-        {
-            in: 'assets/admin/scss/admin.js',
-            out: 'assets/admin/css/admin.min'
-        }
-    ],
+    entryPoints: ['assets/admin/js/admin.js'],
     minify: true,
-    outdir: './',
-    plugins: [watchPlugin],
+    outfile: 'assets/admin/js/admin.min.js',
     sourcemap: true,
     target: 'es5',
 })

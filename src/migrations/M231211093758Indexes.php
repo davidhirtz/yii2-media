fcd2a093c8ed9d33a8136b243b48b73982d754c9
davidhirtz <david.hirtz@me.com>
2024-01-12 15:24:24 +0100

    Update M231211093758Indexes.php

--- src/migrations/M231211093758Indexes.php
+++ src/migrations/M231211093758Indexes.php
@@ -60,7 +60,7 @@ class M231211093758Indexes extends Migration
         $quotedColumns = array_map(fn ($column) => "[[$column]]", $columns);
         $quotedColumns = implode(', ', $quotedColumns);
 
-        $sql = "DELETE FROM $tableName WHERE `id` IN (SELECT `id` FROM $tableName EXCEPT (SELECT MIN(`id`) AS `id` FROM $tableName GROUP BY $quotedColumns) AS `tmp`)";
+        $sql = "DELETE FROM $tableName WHERE `id` NOT IN (SELECT MIN(`id`) FROM (SELECT * FROM $tableName) AS `tmp` GROUP BY $quotedColumns)";
         Yii::$app->getDb()->createCommand($sql)->execute();
 
         $this->createIndex($name, $tableName, $columns, $unique);

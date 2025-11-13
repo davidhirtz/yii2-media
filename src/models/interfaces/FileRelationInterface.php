c445ad29209f1763df250b7f666f98df720ec484
davidhirtz <david.hirtz@me.com>
2025-11-14 00:42:08 +0100

    FileRelationInterface

--- src/models/interfaces/FileRelationInterface.php
+++ src/models/interfaces/FileRelationInterface.php
@@ -6,7 +6,7 @@ namespace davidhirtz\yii2\media\models\interfaces;
 
 use davidhirtz\yii2\media\models\queries\FileQuery;
 use davidhirtz\yii2\skeleton\db\ActiveRecord;
-use davidhirtz\yii2\skeleton\widgets\Widget;
+use yii\base\Widget;
 use yii\db\ActiveRecordInterface;
 
 /**

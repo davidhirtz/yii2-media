d2f657064359c2cd740af1ae3ec2aa74371a8f0f
davidhirtz <david.hirtz@me.com>
2026-09-10 14:59:51 +0200

    Dropdown fix

--- tests/Modules/Admin/Widgets/Grids/Traits/AssetGridViewTraitTest.php
+++ tests/Modules/Admin/Widgets/Grids/Traits/AssetGridViewTraitTest.php
@@ -5,31 +5,49 @@ declare(strict_types=1);
 namespace Hirtz\Media\Tests\Modules\Admin\Widgets\Grids\Traits;
 
 use Hirtz\Media\Modules\Admin\Widgets\Grids\Traits\AssetGridViewTrait;
+use Hirtz\Media\Test\Models\TestAsset;
 use Hirtz\Media\Test\TestCase;
+use Hirtz\Media\Test\Traits\MediaFixtureTrait;
 use Hirtz\Skeleton\Widgets\Grids\GridView;
-use Override;
 
 class AssetGridViewTraitTest extends TestCase
 {
+    use MediaFixtureTrait;
+
     public function testGetDimensionsColumn(): void
     {
-        self::markTestIncomplete();
+        $grid = TestAssetGridView::make();
+
+        $asset = TestAsset::create();
+        $asset->populateFileRelation($this->getFileFromFixture('file-1'));
+
+        self::assertEquals(
+            '<td>1000 x 1000</td>',
+            (string)$grid->getDimensionsColumn()->renderBody($asset, 0, 0),
+        );
+    }
+
+    public function testGetDimensionsColumnWithoutDimensions(): void
+    {
+        $grid = TestAssetGridView::make();
+
+        $file = $this->getFileFromFixture('file-1');
+        $file->width = 0;
+        $file->height = 0;
+
+        $asset = TestAsset::create();
+        $asset->populateFileRelation($file);
+
+        self::assertEquals(
+            '<td>-</td>',
+            (string)$grid->getDimensionsColumn()->renderBody($asset, 0, 0),
+        );
     }
 }
 
 class TestAssetGridView extends GridView
 {
-    use AssetGridViewTrait;
-
-    #[Override]
-    public function configure(): void
-    {
-        $this->provider = $this->getAssetActiveDataProvider();
-
-        $this->columns = [
-            $this->getDimensionsColumn(),
-        ];
-
-        parent::configure();
+    use AssetGridViewTrait {
+        getDimensionsColumn as public;
     }
 }
